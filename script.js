4d1a5e7a2c73b9576e6a24e50985ad074c89ef03
Alequn <alexbentancur132@gmail.com>
2025-04-12 16:12:06 -0300

    ajustes

--- script.js
+++ script.js
@@ -5,39 +5,35 @@ document.addEventListener("DOMContentLoaded", () => {
       btn.addEventListener("click", () => {
         const text = btn.getAttribute("data-copy");
         navigator.clipboard.writeText(text).then(() => {
-          btn.textContent = "Copiado!"; // Cambiar el texto del botón a "Copiado!"
+          btn.textContent = "Copiado!";
           setTimeout(() => {
-            btn.textContent = "Copiar"; // Volver al texto original
+            btn.textContent = "Copiar";
           }, 2000);
-  
-          // Crear la notificación
           showNotification("¡Contenido copiado!");
         });
       });
     });
   
-    // Función para mostrar la notificación
+
+     // sistema de notis
     function showNotification(message) {
-      // Crear un nuevo elemento de notificación
       const notification = document.createElement("div");
       notification.classList.add("notification");
       notification.textContent = message;
   
-      // Agregar la notificación al contenedor
       const container = document.getElementById("notification-container");
       container.appendChild(notification);
   
-      // Mostrar la notificación con animación
       setTimeout(() => {
         notification.classList.add("show");
-      }, 10); // Agregar una pequeña demora para que se vea el efecto de animación
+      }, 10);
   
-      // Eliminar la notificación después de 3 segundos
+      // Eliminar la noti despues de 3 segundos
       setTimeout(() => {
         notification.classList.remove("show");
         setTimeout(() => {
           container.removeChild(notification);
-        }, 500); // Esperar a que termine la animación antes de eliminar
+        }, 500); // Esperar a que termine la animacion antes de eliminar
       }, 3000);
     }
   });
